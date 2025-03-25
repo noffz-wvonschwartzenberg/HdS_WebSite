db30c518877a9d2bb1e773e77c89f36f21884568
Wolfgang von Schwartzenberg <60878610+noffz-wvonschwartzenberg@users.noreply.github.com>
2025-03-25 21:32:46 +0100

    block geändert

--- assets/js/common-js/darkmode.js
+++
@@ -1,8 +0,0 @@
-export function addTheme() {
-    var jsTheme = document.getElementById("js-theme");
-    if (localStorage.getItem("darkMode") == "true") {
-        jsTheme.className = "light-theme";
-    } else {
-        jsTheme.className = "dark-theme";
-    }
-}
