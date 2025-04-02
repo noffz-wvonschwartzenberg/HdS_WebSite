846315c8e2cb1859ed31e82fc8bfa9b7f4c010d5
Wolfgang von Schwartzenberg <60878610+noffz-wvonschwartzenberg@users.noreply.github.com>
2025-04-02 20:10:30 +0200

    pressetexte

--- content/de/WirUeberUns/Presse.md
+++ content/de/WirUeberUns/Presse.md
@@ -11,21 +11,6 @@ Bitte informieren Sie sich in der öffentlichen Presse über uns. Hier finden Si
 <br>
 <div id="article-container"></div>
 
-<details>
-    <summary class="combobox-summary">
-    <p></p>
-    <img src="/img/wirUeberUns/Presse/Rheinische_Post.svg" alt="text" class="image-logo" 
-    style="width: auto; max-width: 100px; height: auto; max-height: 35px;">
-    <p class="article-date" style="font-size: 12px;">Date: 10.1.1000</p>
-    Der Traum des Königs der Sonne
-    </summary>
-    <div class="combobox-details">  
-        <iframe src="/img/wirUeberUns/Presse/DerTraumDesKönigsDerSonne.pdf" title="DerTraumDesKönigsDerSonne" width="100%">
-        Ihr Browser unterstützt keine PDF-Anzeige.
-        </iframe>
-    </div>
-</details>
-
 <script type="module" src="/js/presse/generatePresseHTML.js"></script>
 <script type="module">
     import { generatePresseHTML } from '/js/presse/generatePresseHTML.js';
--- static/js/presse/generatePresseHTML.js
+++ static/js/presse/generatePresseHTML.js
@@ -4,16 +4,36 @@ export function generatePresseHTML()
 {
     let presseHTML = '';
     presse.forEach((press) => {
-        presseHTML += `
-        <div class="article-line"> 
-            <div class="article-column">
-                <img src="${press.Logo}" alt="${press.LogoText}" class="image-logo">
-                <p class="article-date">${press.ArticleDate}</p>
-            </div>
-            <a class="article-title" href="${press.ArticleLink}">${press.ArticleTitle}</a>
-        </div>`;
+        if(press.ArticlePdf === "") {
+            presseHTML += `
+            <div class="article-line"> 
+                <div class="article-column">
+                    <img src="${press.Logo}" alt="${press.LogoText}" class="image-logo">
+                    <p class="article-date">${press.ArticleDate}</p>
+                </div>
+                <a class="article-title" href="${press.ArticleLink}">${press.ArticleTitle}</a>
+            </div>`;
+            console.log("1"); // + press);
+        } else {
+            presseHTML += `
+            <details style="margin 10px;">
+                <summary class="combobox-summary">
+                    <p></p>
+                    <img src="${press.Logo}" alt="${press.LogoText}" class="image-logo" style="width: auto; max-width: 100px; height: auto; max-height: 35px;">
+                    <p class="article-date" style="font-size: 12px;">Date: ${press.ArticleDate}</p>
+                    ${press.ArticleTitle}
+                </summary>
+                <div class="combobox-details">  
+                    <iframe src="${press.ArticlePdf}" title="${press.ArticleTitle}" width="100%">
+                    Ihr Browser unterstützt keine PDF-Anzeige.
+                    </iframe>
+                </div>
+            </details>`;
+            console.log("2"); // + press);
+        }
     });
 
+    console.log("3");
     return presseHTML;
 }
 
