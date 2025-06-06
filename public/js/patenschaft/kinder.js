ee22fb2dbe559e5b8557ea61f261afc784642473
Wolfgang von Schwartzenberg <60878610+noffz-wvonschwartzenberg@users.noreply.github.com>
2025-06-06 19:35:24 +0200

    added public

---
+++ public/js/patenschaft/kinder.js
@@ -0,0 +1,395 @@
+﻿export const kinder = [
+ {
+  "id": "Kind1",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Cheridan.png",
+  "Name": "Cheridan",
+  "Geburtsdatum": "2010",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind2",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Arold-Fosse.png",
+  "Name": "Arold Fosse",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind3",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Daniel-Tchinda.png",
+  "Name": "Daniel Tchinda",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind4",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Djatsa-Shinelda.png",
+  "Name": "Djatsa Shinelda",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind5",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Merveil-Fosso.png",
+  "Name": "Merveil Fosso",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind6",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Stefan.png",
+  "Name": "Stefan",
+  "Geburtsdatum": "2007",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind7",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Diana.png",
+  "Name": "Diana",
+  "Geburtsdatum": "2004",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind8",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Maiva.png",
+  "Name": "Maiva",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind9",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Jean-Gabriel.png",
+  "Name": "Jean Gabriel",
+  "Geburtsdatum": "2013",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind10",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Tsokem-Jovan.png",
+  "Name": "Tsokem Jovan",
+  "Geburtsdatum": "2015",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind11",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Blandine.png",
+  "Name": "Blandine",
+  "Geburtsdatum": "2002",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind12",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Blondel.png",
+  "Name": "Blondel",
+  "Geburtsdatum": "2005",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind13",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Emerson.png",
+  "Name": "Emerson",
+  "Geburtsdatum": "2004",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind14",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Isuel.png",
+  "Name": "Isuel",
+  "Geburtsdatum": "2008",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind15",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Jolanda.png",
+  "Name": "Jolanda",
+  "Geburtsdatum": "2005",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind16",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Beaudrick.png",
+  "Name": "Beaudrick",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind17",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Esperance.png",
+  "Name": "Esperance",
+  "Geburtsdatum": "2007",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind18",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Ivana.png",
+  "Name": "Ivana",
+  "Geburtsdatum": "2008",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind19",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Blanchinelle.png",
+  "Name": "Blanchinelle",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind20",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Fredy.png",
+  "Name": "Fredy",
+  "Geburtsdatum": "2010",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind21",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Pythagore.png",
+  "Name": "Pythagore",
+  "Geburtsdatum": "2008",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind22",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Rodanice.png",
+  "Name": "Rodanice",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind23",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Fevor.png",
+  "Name": "Fevor",
+  "Geburtsdatum": "2013",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind24",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Jeff.png",
+  "Name": "Jeff",
+  "Geburtsdatum": "2014",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind25",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Alan.png",
+  "Name": "Alan",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind26",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Lagloire.png",
+  "Name": "Lagloire",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind27",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Wilfried.png",
+  "Name": "Wilfried",
+  "Geburtsdatum": "2009",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind28",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Laure.png",
+  "Name": "Laure",
+  "Geburtsdatum": "2009",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind29",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Francois-de-Paule.png",
+  "Name": "Francois de Paule",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind30",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Amastel.png",
+  "Name": "Amastel",
+  "Geburtsdatum": "2013",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind31",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Dadiana.png",
+  "Name": "Dadiana",
+  "Geburtsdatum": "2002",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind32",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Freichnel.png",
+  "Name": "Freichnel",
+  "Geburtsdatum": "2008",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind33",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Riopsel.png",
+  "Name": "Riopsel",
+  "Geburtsdatum": "2010",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind34",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Bryan.png",
+  "Name": "Bryan",
+  "Geburtsdatum": "2007",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind35",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Gigi.png",
+  "Name": "Gigi",
+  "Geburtsdatum": "2013",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind36",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Momo-Blessing.png",
+  "Name": "Momo Blessing",
+  "Geburtsdatum": "2014",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind37",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Samuel-Kami-Nyankam.png",
+  "Name": "Samuel Kami Nyankam",
+  "Geburtsdatum": "2019",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind38",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Christiane.png",
+  "Name": "Christiane",
+  "Geburtsdatum": "2014",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind39",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Tafopa-Bridinette.png",
+  "Name": "Tafopa Bridinette",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind40",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Patrick.png",
+  "Name": "Patrick",
+  "Geburtsdatum": "2010",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind41",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Belva.png",
+  "Name": "Belva",
+  "Geburtsdatum": "2007",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind42",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Tiaya-Willito.png",
+  "Name": "Tiaya Willito",
+  "Geburtsdatum": "2012",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind43",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Estrella.png",
+  "Name": "Estrella",
+  "Geburtsdatum": "2007",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind44",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Keudong-Brayan.png",
+  "Name": "Keudong Brayan",
+  "Geburtsdatum": "2010",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind45",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Kenne-Jeff.png",
+  "Name": "Kenne Jeff",
+  "Geburtsdatum": "2013",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind46",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Raissa.png",
+  "Name": "Raissa",
+  "Geburtsdatum": "2008",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": true
+ },{
+  "id": "Kind47",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Aurine.png",
+  "Name": "Aurine",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": false
+ },{
+  "id": "Kind48",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Brendalice.png",
+  "Name": "Brendalice",
+  "Geburtsdatum": "2006",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": false
+ },{
+  "id": "Kind49",
+  "Bild": "/img/Versorgung/EineWarmeMahlzeit/Kinder/Tiwa-Bradelle.png",
+  "Name": "Tiwa Bradelle",
+  "Geburtsdatum": "2011",
+  "Geburtsort": "Mbouda",
+  "KurzeBeschreibung": "",
+  "HatEinenPaten": false
+ }
+];
